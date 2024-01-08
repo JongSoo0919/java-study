80489c44522917064ad1c81b7321d0551a37751d
KimJongSoo <prjs3643@gmail.com>
2024-01-08 23:12:16 +0900

    변수 문제풀이

---
+++ src/variable/solve/VarTest2.java
@@ -0,0 +1,12 @@
+package variable.solve;
+
+public class VarTest2 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+        int answer = num1 + num2;
+        System.out.println("answer = " + answer);
+
+    }
+}
---
+++ src/variable/solve/VarTest3.java
@@ -0,0 +1,12 @@
+package variable.solve;
+
+public class VarTest3 {
+    public static void main(String[] args) {
+        long l = 10000000000L;
+        boolean b = true;
+
+        System.out.println("l = " + l);
+        System.out.println("b = " + b);
+
+    }
+}
