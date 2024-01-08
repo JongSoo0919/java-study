b776c0a44e551d4964a20c8ea54fd169d3019cc7
KimJongSoo <prjs3643@gmail.com>
2024-01-08 23:05:09 +0900

    변수 이론

---
+++ src/variable/Var7.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var7 {
+    public static void main(String[] args) {
+        int a = 10; // 정수
+        double d = 10.5; // 실수
+        boolean b = true; // 참 or 거짓
+        char c = 'A'; // 문자 하나
+        String s = "str"; // 문자열 (특수 타입)
+
+        System.out.println("a = " + a);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
---
+++ src/variable/Var8.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        byte b = 127;
+        short s = 32767;
+        int i = 21;
+
+    }
+}
