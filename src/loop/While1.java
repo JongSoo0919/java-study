b4acda8f4f54858e9dc1b5026812067cd2a4b675
KimJongSoo <prjs3643@gmail.com>
2024-01-13 23:39:54 +0900

    반복문기초

---
+++ src/loop/While1.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class While1 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        for (int i = 0; i < 3; i++) {
+            count++;
+        }
+        System.out.println(count);
+
+    }
+}
