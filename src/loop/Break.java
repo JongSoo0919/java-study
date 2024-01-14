037ff6b0253b46c1aa44de3958cf2cb720fd9949
KimJongSoo <prjs3643@gmail.com>
2024-01-14 22:33:24 +0900

    반복문기초

---
+++ src/loop/Break.java
@@ -0,0 +1,17 @@
+package loop;
+
+public class Break {
+    public static void main(String[] args) {
+        int sum = 0;
+        int i = 0;
+
+        while(true){
+            sum += i;
+            i++;
+            if (sum > 10) {
+                break;
+            }
+            System.out.println(sum);
+        }
+    }
+}
---
+++ src/loop/DoWhile1.java
@@ -0,0 +1,11 @@
+package loop;
+
+public class DoWhile1 {
+    public static void main(String[] args) {
+        int i = 0;
+
+        do {
+            System.out.println("원래는 실행되면 안됨");
+        } while (i > 3);
+    }
+}
