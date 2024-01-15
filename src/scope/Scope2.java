f77ac676de7c62120abddfb12aa990962d052bed
KimJongSoo <prjs3643@gmail.com>
2024-01-15 23:02:12 +0900

    스코프 기초

---
+++ src/scope/Scope2.java
@@ -0,0 +1,14 @@
+package scope;
+
+public class Scope2 {
+    public static void main(String[] args) {
+        int m = 10;
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println("m = " + m);
+            System.out.println("i = " + i);
+        } // i 생존 종료
+
+        System.out.println("m = " + m);
+    }// m 생존 종료
+}
