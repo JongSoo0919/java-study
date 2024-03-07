f756e4aa8d85998745679d60a5ae79b42969e227
KimJongSoo <prjs3643@gmail.com>
2024-03-07 18:09:46 +0900

    패키지 기초 - 패키지 구조 예시

---
+++ src/com/helloshop/product/Product.java
@@ -0,0 +1,11 @@
+package com.helloshop.product;
+
+public class Product {
+    String productName;
+    int quantity;
+
+    public Product(String productName, int quantity) {
+        this.productName = productName;
+        this.quantity = quantity;
+    }
+}
---
+++ src/com/helloshop/user/User.java
@@ -0,0 +1,11 @@
+package com.helloshop.user;
+
+public class User {
+    String userName;
+    int age;
+
+    public User(String userName, int age) {
+        this.userName = userName;
+        this.age = age;
+    }
+}
