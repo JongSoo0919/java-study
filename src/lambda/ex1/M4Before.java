5905910b56b077b01643f04eb3806f3a7786141e
KimJongSoo <js.kim3@okestro.com>
2025-04-18 17:58:01 +0900

    30. 람다 - 문제1

---
+++ src/lambda/ex1/M4Before.java
@@ -0,0 +1,40 @@
+package lambda.ex1;
+
+import java.util.Arrays;
+
+public class M4Before {
+    static interface Run {
+        void run();
+    }
+
+    public static void main(String[] args) {
+        Run run1 = () -> {
+            int N = 100;
+            long sum = 0;
+            for (int i = 1; i <= N; i++) {
+                sum += i;
+            }
+
+            System.out.println("[1부터 100까지의 합] 결과: " + sum);
+        };
+
+        Run run2 = () -> {
+            int[] arr = {4, 3, 2, 1};
+            System.out.println("원본 배열: " + Arrays.toString(arr));
+            Arrays.sort(arr);
+            System.out.println("배열 정렬: " + Arrays.toString(arr));
+        };
+
+        run(run1);
+        System.out.println();
+        run(run2);
+
+    }
+
+    public static void run(Run run) {
+        long start = System.currentTimeMillis();
+        run.run();
+        long end = System.currentTimeMillis();
+        System.out.println("실행 시간: " + (end-start) +"ns");
+    }
+}
---
+++ src/lambda/ex1/MyFunction.java
@@ -0,0 +1,6 @@
+package lambda.ex1;
+
+@FunctionalInterface
+public interface MyFunction {
+    int apply(int a, int b);
+}
