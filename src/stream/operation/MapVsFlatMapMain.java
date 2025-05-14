23721bc5272038f49da5b6f72bbb7a699848bdcb
KimJongSoo <prjs3643@gmail.com>
2025-05-14 23:32:24 +0900

    34. Stream API 기능 - 3중 중첩 리스트 처리

--- src/stream/operation/MapVsFlatMapMain.java
+++ src/stream/operation/MapVsFlatMapMain.java
@@ -1,6 +1,8 @@
 package stream.operation;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,5 +38,41 @@ public class MapVsFlatMapMain {
                         .map(i -> i * 2))
                 .toList();
         System.out.println(list2);
+
+        List<List<List<String>>> company = List.of(
+                // 회사 A
+                List.of(
+                        List.of("Alice", "Bob"),       // 부서 A-1
+                        List.of("Charlie", "Diana")    // 부서 A-2
+                ),
+                // 회사 B
+                List.of(
+                        List.of("Eve", "Frank"),        // 부서 B-1
+                        List.of("Jerry", "Tom")         // 부서 B-2
+                )
+        );
+        // 회사 A의 부서 A-2의 2번째 직원 Diana
+        System.out.println(company.get(0).get(1).get(1));
+
+        //flatMap 으로 표현
+        // 부서를 없애고, 회사로 표현
+        List<List<String>> company2 = company.stream()
+                .flatMap(list -> list.stream())
+                .toList();
+        System.out.println(company2);
+
+        List<String> company3 = company.stream()
+                .flatMap(list -> list.stream())
+                .flatMap(dept -> dept.stream())
+                .map(String::toUpperCase)
+                .toList();
+        System.out.println(company3);
+
+        List<String> company4 = company.stream()
+                .flatMap(list -> list.stream()
+                        .flatMap(dept -> dept.stream()))
+                .filter(name -> name.startsWith("A"))
+                .toList();
+        System.out.println(company4);
     }
 }
