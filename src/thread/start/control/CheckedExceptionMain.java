c4ef1b010acf2bda2f18d017c81d33a0d23362ac
KimJongSoo <prjs3643@gmail.com>
2025-11-17 23:00:43 +0900

    40. 쓰레드의 체크 예외

---
+++ src/thread/start/control/CheckedExceptionMain.java
@@ -0,0 +1,16 @@
+package thread.start.control;
+
+import static util.ThreadUtils.sleep;
+
+public class CheckedExceptionMain {
+    public static void main(String[] args) throws Exception{
+        throw new Exception();
+    }
+
+    static class CheckRunnable implements Runnable {
+        @Override
+        public void run() {
+            sleep(1000);
+        }
+    }
+}
