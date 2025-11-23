72d7aefefef9a04e29e1a390f1dd3fd175f3fecc
KimJongSoo <prjs3643@gmail.com>
2025-11-23 22:21:56 +0900

    40. 프린터 예제

---
+++ src/thread/start/control/printer/MyPrinterV2.java
@@ -0,0 +1,58 @@
+package thread.start.control.printer;
+
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class MyPrinterV2 {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Thread printerThread = new Thread(printer, "printer");
+        printerThread.start();
+
+        Scanner userInput = new Scanner(System.in);
+        while(true) {
+            log("프린터할 문서를 입력하세요. 종료 (q): ");
+            String input = userInput.nextLine();
+            if(input.equals("q")) {
+                log("작업 중단 지시 thread.interrupt()");
+                printerThread.interrupt();
+                break;
+            }
+            printer.addJob(input);
+        }
+    }
+
+    static class Printer implements Runnable {
+        volatile boolean work = true;
+        Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
+
+        @Override
+        public void run() {
+            try {
+                while (!Thread.interrupted()) {
+                    if (jobQueue.isEmpty()) {
+                        continue;
+                    }
+
+                    String job = jobQueue.poll();
+                    log("출력 시작" + job + ", 대기 문서 : " + jobQueue);
+                    Thread.sleep(3000);
+                    log("출력 완료");
+
+                }
+            } catch (InterruptedException e){
+                log("interrupted : 프린터 종료");
+            }
+
+            log("프린터 완료");
+        }
+
+        public void addJob(String input) {
+            jobQueue.add(input);
+        }
+    }
+}
