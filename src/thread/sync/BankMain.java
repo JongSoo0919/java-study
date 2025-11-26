e20102dd5d6d55ecad20c7be647c4d8b8b8657cd
KimJongSoo <prjs3643@gmail.com>
2025-11-26 23:39:01 +0900

    42. 동시성 에러 코드 작성

---
+++ src/thread/sync/BankMain.java
@@ -0,0 +1,24 @@
+package thread.sync;
+
+import static util.MyLoggerThread.log;
+import static util.ThreadUtils.sleep;
+
+public class BankMain {
+    public static void main(String[] args) throws InterruptedException {
+        BankAccount account = new BankAccountV1(1000);
+
+        Thread t1 = new Thread(new WithdrawTask(account, 800), "t1");
+        Thread t2 = new Thread(new WithdrawTask(account, 800), "t2");
+        t1.start();
+        t2.start();
+
+        sleep(500); // 검증 완료까지 잠시 대기
+        log("t1 state: " + t1.getState());
+        log("t2 state: " + t2.getState());
+
+        t1.join(); // 최종 잔액 확인을 위한, 쓰레드 종료 대기
+        t2.join();
+
+        log("최종 잔액 : " + account.getBalance());
+    }
+}
