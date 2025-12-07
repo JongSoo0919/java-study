9738bd54470476438ac661862d8952741fb2e2da
KimJongSoo <prjs3643@gmail.com>
2025-12-07 21:19:41 +0900

    43. 고급 동기화 - concurrent.Lock - LockSupport - ReentrantLock

---
+++ src/thread/sync/BankAccountV4.java
@@ -0,0 +1,50 @@
+package thread.sync;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+import static util.MyLoggerThread.log;
+import static util.ThreadUtils.sleep;
+
+public class BankAccountV4 implements BankAccount{
+    private int balance;
+
+    private final Lock lock = new ReentrantLock();
+
+    public BankAccountV4(int initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public boolean withdraw(int amount) {
+        log("거래 시작 : " + getClass().getSimpleName());
+
+        // 잔고가 출금액 보다 적으면 진행하면 안됨.
+        log("[검증 시작] 출금액 : " + amount + ", 잔액: " + balance);
+
+        lock.lock(); // ReentrantLock을 이용하여 임계 영역을 설정
+        try {
+            if (balance < amount) {
+                log("[검증 실패]");
+                return false;
+            }
+
+            // 잔고가 출금액 보다 많으면 진행
+            log("[검증 완료] 출금액 : " + amount + ", 잔액: " + balance);
+            sleep(1000); // 출금에 걸리는 시간으로 가정
+            balance -= amount;
+
+            log("[출금 완료] 출금액 : " + amount + ", 잔액: " + balance);
+        } finally {
+            lock.unlock();
+        }
+
+        log("거래 종료");
+        return true;
+    }
+
+    @Override
+    public int getBalance() {
+        return balance;
+    }
+}
